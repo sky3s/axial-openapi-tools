5d1fbf61439e72a8b849b9985b6711c1d5adab8f
Selman Kaya <kayaselmann@gmail.com>
2023-01-16 21:46:30 +0300

    ApiHeader and related stuffs removed.

--- src/main/java/com/axial/modules/openapi_tools/SpringDocInitializer.java
+++ src/main/java/com/axial/modules/openapi_tools/SpringDocInitializer.java
@@ -18,59 +18,21 @@ public class SpringDocInitializer {
 
     private final ConfigurableListableBeanFactory beanFactory;
 
-    private final ApiCustomizer apiCustomizer;
-
     private final ApplicationApiConfig applicationApiConfig;
 
     private final SpringDocCustomizerActions customizerActions;
 
     public SpringDocInitializer(ConfigurableListableBeanFactory beanFactory,
-                                ApiCustomizer apiCustomizer,
                                 ApplicationApiConfig applicationApiConfig,
                                 SpringDocCustomizerActions customizerActions) {
         this.beanFactory = beanFactory;
-        this.apiCustomizer = apiCustomizer;
         this.applicationApiConfig = applicationApiConfig;
         this.customizerActions = customizerActions;
     }
 
-
     @PostConstruct
     private void initOps() {
-        createApiData();
         addCustomizers();
-    }
-
-    private void createApiData() {
-
-        /**
-         * Adding default headers to ApiData
-         */
-        ApiData.addApiHeadersToAppHeadersIfNotExist(apiCustomizer.getHeaders());
-        ApiData.addApiHeadersToAppHeadersIfNotExist(apiCustomizer.getApiHeaders());
-        ApiData.addSecurityHeaderConfigsToAppHeadersIfNotExist(apiCustomizer.getSecurityHeaders());
-
-        /**
-         * Adding default YML Headers to ApiData
-         */
-        ApiData.addHeaderConfigsToAppHeadersIfNotExist(
-                OpenApiDataUtils.emptyIfNull(applicationApiConfig.getCommonHeaders()).values().stream().toList());
-        ApiData.addSecurityHeaderConfigsToAppHeadersIfNotExist(
-                OpenApiDataUtils.emptyIfNull(applicationApiConfig.getCommonSecurityHeaders()).values().stream().toList());
-
-        /**
-         * Adding API Specific YML Headers to ApiData
-         */
-        ApiData.addHeaderConfigsToAppHeadersIfNotExist(
-                applicationApiConfig.getApis().values().stream()
-                        .flatMap(api ->
-                                OpenApiDataUtils.emptyIfNull(api.getHeaders()).values().stream())
-                        .collect(Collectors.toList()));
-        ApiData.addSecurityHeaderConfigsToAppHeadersIfNotExist(
-                applicationApiConfig.getApis().values().stream()
-                        .flatMap(api ->
-                                OpenApiDataUtils.emptyIfNull(api.getSecurityHeaders()).values().stream())
-                        .collect(Collectors.toList()));
     }
 
     private void addCustomizers() {
@@ -91,7 +53,6 @@ public class SpringDocInitializer {
                                 .add(openApi ->
                                         customizerActions.customizeOpenAPI(openApi, apiMap.get(groupedOpenApi.getGroup())))
                 );
-
     }
 
 }
